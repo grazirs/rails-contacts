63010909953aa6b34eaa373a709bf91f86d5717e
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-02-03 10:43:15 -0300

    add validations to each model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,17 @@
 class Contact < ApplicationRecord
   belongs_to :user
+  has_many :phones
+
+  validates :name, presence: true
+  validates :birth_date, format: { with: /^\d{2}\/\d{2}\/\d{4}$/ }
+  validate :validate_unique_name
+
+  private
+
+  def validate_unique_name
+    contact = Contact.find_by(user_id: user_id, name: name)
+    if contact != nil
+      errors.add(:name, "Name already exists!")
+    end
+  end
 end
--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,3 +1,15 @@
 class Phone < ApplicationRecord
   belongs_to :contact
+
+  validates :number, presence: true, format: { with: /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/ }
+  validate :validate_phone_type
+  validates :main_phone, inclusion: [true, false]
+
+  private
+
+  def validate_phone_type
+    if !phone_type.match(/^home$|^work$|^other$/)
+    errors.add(:phone_type, "Invalid phone type")
+    end
+  end
 end
