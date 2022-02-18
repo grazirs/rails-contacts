20dd47c4e170af80a5ed237cec7963168009157c
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-02-18 15:43:02 -0300

    add show test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,53 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+
+  fixtures :users, :contacts, :phones
+
   describe "GET /index" do
-    it "returns http success" do
-      get "/users/index"
+    it "should returns http success" do
+      get "/users"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
-    it "returns http success" do
-      get "/users/show"
+    it "should returns http success" do
+      get "/users/1"
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe "GET /new" do
-    it "returns http success" do
-      get "/users/new"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/users/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/users/edit"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /update" do
-    it "returns http success" do
-      get "/users/update"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/users/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
+  
 end
